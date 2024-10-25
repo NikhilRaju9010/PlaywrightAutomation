904108f03ec69146415aad0b23a3aa1d7da18daa
Nikhil Raju <nikhilrajutirlange@gmail.com>
2024-10-25 17:49:53 +0530

    Created new file to genertae the random weight for the webdriver

---
+++ tests/randomWaitGenerator.spec.ts
@@ -0,0 +1,8 @@
+export class RandomWeightGenerator {
+    private static waitNumbers: number[] = [1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500, 5000];
+
+    public static getRandomWeight(): number {
+        const randomIndex = Math.floor(Math.random() * this.waitNumbers.length);
+        return this.waitNumbers[randomIndex];
+    }
+}
